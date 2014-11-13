296f8412a3bf13ce1d0efca67fcf7b3b3505aed4
Swapneel Sheth <swapneel@cis.upenn.edu>
2014-11-13 18:53:24 -0500

    hurray.. unit testing

---
+++ NameTest.java
@@ -0,0 +1,28 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class NameTest {
+
+        protected Name name;
+
+        @Before
+        public void setUp() throws Exception {
+        }
+
+
+        @Test
+        public void testName() {
+             name = new Name("Dr", "Homer", "J.", "Simpson", "Simpson", "Bouvier", "Jr", "Dr");
+
+             String actual = name.getFirst();
+             String expected = "Homer";
+
+             assertEquals("getFirst returned wrong value", expected, actual);
+
+            }
+        }
+
+}
