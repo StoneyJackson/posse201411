e48528b56a92300046f60303d4b7e3734b5e5c83
Barrett Koster <kosterb@meredith.edu>
2014-11-13 18:52:07 -0500

    add maternal to the test, fix professional to suffixProfessional in getSuffixProfessional

--- Name.java
+++ Name.java
@@ -55,7 +55,7 @@ public class Name {
     }
 
     public String getSuffixProfessional() {
-        return professional;
+        return suffixProfessional;
     }
 
     public String getSuffixGenerational() {
